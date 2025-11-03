ce7800d9d59ab122ebf71f6b779d28f8cadd022a
yesung1215 <xocds991215@gmail.com>
2025-11-03 10:19:04 +0900

    feat(post): 공유_임시저장

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { ThemeProvider } from 'styled-components';
-import './App.css';
-import GlobalStyle from './styles/global';
-import theme from './styles/theme';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/router';
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
+import { ModalProvider } from "./components/modal/useModal";
+import ConfirmModal from "./components/modal/ConfirmModal";
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <RouterProvider router={router}/>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <ModalProvider>
+        <RouterProvider router={router} />
+        <ConfirmModal /> 
+      </ModalProvider>
+    </ThemeProvider>
   );
 }
 
