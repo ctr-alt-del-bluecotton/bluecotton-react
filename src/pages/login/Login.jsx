e5caea543c6df7728349a70666eabc60905bbb50
gksals12 <yunhanmin@gmail.com>
2025-11-03 09:48:12 +0900

    feat(login): 유효성검사 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,26 +1,83 @@
 import React from "react";
-import * as S from "./style";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import S from "./style";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: {errors, isSubmitting}
+  } = useForm({mode: "onChange"});
+
+  const handleSubmitForm = handleSubmit(async (data) => {
+    const {memberPasswordConfirm, ...member} = data;
+
+    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/member/login`,{
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(member)
+    })
+
+    // 백앤드 연결시 활성화
+    // if(!res.ok){
+    //   alert("이메일 또는 비밀번호가 일치하지 않습니다");
+    //   return;
+    // }
+    navigate("/main/som/all")
+  })
+
+  //  로그인 유효성검사
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
+
   return (
     <S.LoginContainer>
       
       {/* background */}
       <S.BackgroundBox />
 
-      {/* login box */}
-      <S.LoginBox>
-        
-        {/* logo */}
-        <S.Logo>blue cotton</S.Logo>
-        
-        {/* input */}
-        <S.Input type="text" placeholder="이메일을 작성해주세요" name="memberEmail" />
-        <S.Input type="password" placeholder="비밀번호를 작성해주세요" name="memberPassword" />
-        
-        {/* login button */}
-        <S.LoginButton to="/main/som/all">로그인하기</S.LoginButton>
+        <S.LoginBox>
+        {/* login box */}
+          <S.LoginForm onSubmit={handleSubmitForm}>
+            {/* logo */}
+            <S.Logo>blue cotton</S.Logo>   
+
+            {/* input */}
+            <S.Input type="text" placeholder="이메일을 작성해주세요" name="memberEmail" 
+            {...register("memberEmail", {
+              required : true,
+              pattern : {
+                value : emailRegex
+              }
+            })}
+            />
+            {errors.memberEmail && (
+              <S.InputErrorMessage>올바른 이메일을 입력해주세요</S.InputErrorMessage>
+            )}            
+
+            <S.Input type="password" placeholder="비밀번호를 작성해주세요" name="memberPassword"
+            {...register("memberPassword", {
+              required : true,
+              pattern : {
+                value : passwordRegex
+              }
+            })} 
+            />
+
+            {errors && errors?.memberPassword?.type === "required" && (
+              <S.InputErrorMessage>비밀번호를 입력하세요.</S.InputErrorMessage>
+            )}
+            {errors && errors?.memberPassword?.type === "pattern" && (
+              <S.InputErrorMessage>소문자, 숫자, 특수문자를 각 하나 포함한 8자리 이상이여야 합니다.</S.InputErrorMessage>
+            )}   
+
+            {/* login button */}
+            <S.LoginButton as="button" type="submit" disabled={isSubmitting}>로그인하기</S.LoginButton>
+          </S.LoginForm>
 
         <S.Divider />
 
